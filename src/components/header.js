48ec1a3d0005f5e40098f831654fa305c9368193
Arturo Cullinane <psychdramatic2@gmail.com>
2018-05-30 12:23:22 +0100

    Add header component and avatar

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,69 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+const Header = props => {
+	const bio = props.bio;
+	const username = props.username;
+	const src =  props.src
 
-export default Header
+	return (
+		<div
+			style={ {
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexWrap: 'wrap',
+				margin: '2em 2em',
+			} }
+		>
+			<span
+				style={ {
+					flexBasis: '120px',
+					height: '96px',
+				} }
+			>
+				<img
+					src={ src }
+					alt={ `Psychdramatic` }
+					style={ {
+						marginBottom: 0,
+						borderRadius: "50%",
+						width: '96px',
+					} }
+				/>
+			</span>
+			<span
+				style={ {
+					flexBasis: '500px',
+					flexGrow: 1,
+				} }
+			>
+				<Link
+					to='/'
+					activeStyle={ {
+        				textDecoration: 'none',
+        				color: '#000000'	
+      				} }
+				>
+					<h3
+						dangerouslySetInnerHTML={ { __html: ( username ) } }
+						style={ { marginBottom: '0.2em' } }
+					/>
+				</Link>
+				<p 
+					style={ { marginBottom: 0 } }
+					dangerouslySetInnerHTML={ { __html: ( bio ) } }
+				/>
+			</span>
+		</div>
+	)
+}
+
+Header.PropTypes = {
+	src: PropTypes.string,
+	username: PropTypes.string,
+	bio: PropTypes.string,
+}
+
+export default Header;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,28 +5,37 @@ import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const Layout = props => {
+
+  const children = props.children;
+  const data = props.data;
+  const bio = props.data.allWordpressWpMe.edges[0].node.description;
+	const username = props.data.allWordpressWpMe.edges[0].node.name;
+  const avatar = props.data.allWordpressWpMe.edges[0].node.avatar_urls.wordpress_96;
+
+  return (
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Header bio={bio} username={username} src={avatar} siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
@@ -34,11 +43,22 @@ Layout.propTypes = {
 
 export default Layout
 
-export const query = graphql`
-  query SiteTitleQuery {
+export const pageQuery = graphql`
+  query IndexQuery {
     site {
       siteMetadata {
         title
+      }
+    }
+    allWordpressWpMe {
+      edges {
+        node {
+          name
+          description
+          avatar_urls {
+            wordpress_96
+          }
+        }
       }
     }
   }
